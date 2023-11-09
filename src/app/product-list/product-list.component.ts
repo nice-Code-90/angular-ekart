68d4029228d2d1d601b28a5f2e9f6e9c620a1ba2
Milan Mazik <mazik1990@hotmail.com>
2023-11-09 22:54:50 +0100

    Product image+Buy now BTN: property binding
    property binding for shows data on HTML template from component class properties

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,12 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
+  name = 'John Doe';
   product = {
     name: 'iPhone X',
     price: 789,
     color: 'Black',
     discount: 8.5,
     inStock: 4,
+    pImage: '/assets/images/iphone.jpg',
   };
 
   getDiscountedPrice() {
